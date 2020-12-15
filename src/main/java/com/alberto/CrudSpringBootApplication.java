e48482122405944203ec91c8a6c640aea31e4cd0
Alberto Hernandez <albertohernanrodri@gmail.com>
2020-12-15 17:35:48 +0100

    First Commit

--- src/main/java/com/alberto/CrudSpringBootApplication.java
+++ src/main/java/com/alberto/CrudSpringBootApplication.java
@@ -8,6 +8,7 @@ public class CrudSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudSpringBootApplication.class, args);
+		System.out.println("Hola que tal estas");
 	}
 
 }
